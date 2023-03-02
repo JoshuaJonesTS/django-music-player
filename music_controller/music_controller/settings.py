1b8eb9df120e1d101c51629f474302992d3df37e
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-03-01 20:47:11 -0800

    Change settings configs
    This got rid of terminal errors.

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABSE_USER'),
-        'PASSWORD': env('DATABASE_PASS')      
+        'NAME': BASE_DIR / env('DATABASE_NAME'),
+        # 'USER': env('DATABASE_USER'),
+        # 'PASSWORD': env('DATABASE_PASS')      
     }
 }
 
